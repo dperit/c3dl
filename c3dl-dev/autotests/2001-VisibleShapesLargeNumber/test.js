ae4d622dda02432282427ad53be05725a005686d
unknown <dperit@.(none)>
2011-05-20 13:36:12 -0400

    Temporarycommit

--- c3dl-dev/autotests/2001-VisibleShapesLargeNumber/test.js
+++ c3dl-dev/autotests/2001-VisibleShapesLargeNumber/test.js
@@ -1,5 +1,65 @@
-c3dl.addMainCallBack(test, "shape_test");
-c3dl.addModel("../models/sphere.dae");
+c3dl.addMainCallBack(test, "visibleShapesLargeNumber_test");
+var xLimit = 10;
+var yLimit = 10;
+var zLimit = 10;
+
+var xSize = 1;
+var ySize = 1;
+var zSize = 1;
+
+var xScale = 1.6;
+var yScale = 1.6;
+var zScale = -1.6;
+
+var totalFPS = 0;
+var numFPS = 0;
+var timeSinceLastChange = 0;
+var duck, scn;
+var testDone = false;
+
+
+
+function test(canvasName){
+  scn = new c3dl.Scene();
+  scn.setCanvasTag(canvasName);
+  renderer = new c3dl.WebGL();
+  renderer.createRenderer(this);
+  scn.setRenderer(renderer);
+  scn.init(canvasName);
+  
+  if(renderer.isReady()) {   
+    for(var x = 0; x < xLimit; x++){
+      for(var y = 0; y < yLimit; y++){
+        for (var z = 0; z < zLimit; z++){
+          var curCube = new c3dl.Cube(1,1,1);
+          curCube.setPosition([(x * xScale) - ((xLimit * xScale)/2),
+                               y* yScale- ((yLimit * yScale)/2),
+                               z * zScale]);
+          //curCube.setAngularVel([0.0, -0.001, 0.0]);
+          scn.addObjectToScene(curCube);
+        }
+      }
+    }
+    
+    var cam = new c3dl.FreeCamera();
+    cam.setPosition([0, 0, 30.0]);
+    cam.setLookAtPoint([0, 0, 0]);
+    scn.setCamera(cam);
+    scn.setUpdateCallback(checktime);
+    scn.startScene();
+  }
+}
+
+function checktime(time){
+  var curFPS = scn.getFPS();
+  document.getElementById('fps').innerHTML = curFPS;
+  totalFPS+=curFPS;
+  numFPS++;
+  document.getElementById('avgfps').innerHTML = totalFPS/numFPS;
+  
+}
+
+//
 //function test(canvasName, callback){
 //  scn = new c3dl.Scene();
 //  scn.setCanvasTag(canvasName);
@@ -9,38 +69,17 @@ c3dl.addModel("../models/sphere.dae");
 //  scn.init(canvasName);
 //  
 //  if (renderer.isReady()) {
-//    var cube = new c3dl.Cube(5,5,5);
-//    cube.setTexture("../models/images/testing.jpg");
+//    var plane = new c3dl.Plane(5,5);
+//    plane.setTexture("../models/images/testing.jpg");
 //    var cam = new c3dl.FreeCamera();
-//    cam.setPosition([0.0, 0.0, 15.0]);
+//    cam.setPosition([0.0, 3, 9]);
 //    cam.setLookAtPoint([0.0, 0.0, 0.0]);
-//    scn.addObjectToScene(cube);
+//    scn.addObjectToScene(plane);
 //    scn.setCamera(cam);
 //    scn.startScene();
 //    callback();
 //  }
 //}
-//
-function test(canvasName, callback){
-  scn = new c3dl.Scene();
-  scn.setCanvasTag(canvasName);
-  renderer = new c3dl.WebGL();
-  renderer.createRenderer(this);
-  scn.setRenderer(renderer);
-  scn.init(canvasName);
-  
-  if (renderer.isReady()) {
-    var plane = new c3dl.Cube(5,5,5);
-    plane.setTexture("../models/images/testing.jpg");
-    var cam = new c3dl.FreeCamera();
-    cam.setPosition([5.0, 3, 9]);
-    cam.setLookAtPoint([0.0, 0.0, 0.0]);
-    scn.addObjectToScene(plane);
-    scn.setCamera(cam);
-    scn.startScene();
-    try{callback()}catch(err){};
-  }
-}
 //
 //function test(canvasName, callback){
 //  scn = new c3dl.Scene();
